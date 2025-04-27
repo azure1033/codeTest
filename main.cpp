3836b41c6ef1fa9be4a70c76f6327a08563a83a3
azure1033 <2780019661@qq.com>
2025-04-27 20:36:59 +0800

    learn a bit of for_each usage

--- main.cpp
+++ main.cpp
@@ -4,27 +4,43 @@
 
 #include <gtest/gtest.h>
 #include <iostream>
-#include <string>
-#include <stack>
+// #include <string>
+#include <vector>
+#include <climits>
+#include <numeric>
+#include <algorithm>
+
+using namespace std;
+
+// void print(int a, const int& b) {
+//     std::cout << a << " " << b << std::endl;
+// }
+
+// TEST(isprimes, test1) {
+//     std::vector<int> test;
+//     getData(test);
+//     std::vector<bool> expected = {true, true, true, false, false, true, false, false};
+
+//     for (size_t i = 0; i < test.size(); ++i) {
+//         EXPECT_EQ(isprimes(test[i]), expected[i]) << "Test failed for input: " << i;
+//     }
+// }
 
 
-
-void print(int a, const int& b) {
-    std::cout << a << " " << b << std::endl;
-}
-
-TEST(isprimes, test1) {
-    std::vector<int> test;
-    getData(test);
-    std::vector<bool> expected = {true, true, true, false, false, true, false, false};
-
-    for (size_t i = 0; i < test.size(); ++i) {
-        EXPECT_EQ(isprimes(test[i]), expected[i]) << "Test failed for input: " << i;
-    }
-}
 
 int main() {
-    ::testing::InitGoogleTest();
-    return RUN_ALL_TESTS();
+    // ::testing::InitGoogleTest();
+    // return RUN_ALL_TESTS();
+
+    vector<int> arr;
+    getData(arr);
+
+    int m = INT_MIN;
+
+    for_each(arr.begin(), arr.end(), [&](int a) {
+        m = max(m, a);
+        cout << "m: " << m << '\n';
+    });
+    cout << m;
     return 0;
 }
